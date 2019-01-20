819499c1589047ac7956ac808c00037d5c2c2ea1
Supanat J <supanat.ji@gmail.com>
2019-01-20 11:52:06 +0700

    udp client server

---
+++ 01-Intro/UDPClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String server = "127.0.0.1"; // loop back address
+            int servPort = 5000;
+
+            String message = "Supanat";
+
+            byte[] sendPacket = Encoding.ASCII.GetBytes(message);
+
+            UdpClient client = new UdpClient();
+
+            try{
+
+                client.Send(sendPacket,sendPacket.Length,server,servPort);
+
+            }catch(SocketException e){
+                Console.WriteLine(e.ErrorCode+" "+e.Message);
+            }
+
+
+        }
+    }
+}
---
+++ 01-Intro/UDPServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int servPort = 5000;
+            UdpClient client = null;
+
+            client = new UdpClient(servPort);
+
+            IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any,0);
+
+            for(;;){
+
+                try{
+
+                    byte[] byteBuffer = client.Receive(ref remoteIPEndPoint);
+
+                    String recvMessage = Encoding.ASCII.GetString(byteBuffer,0,byteBuffer.Length);
+
+                    Console.WriteLine(recvMessage);
+
+                }catch(SocketException e){
+                    Console.WriteLine(e.ErrorCode+" "+e.Message);
+                }
+            }
+        }
+    }
+}
